37e7c418da365c5d259bc6daab2b5785767d0452
sharks <sharks@localhost.localdomain>
2019-09-15 19:46:08 -0500

    remove specific benchmark

--- after-success.sh
+++ after-success.sh
@@ -8,11 +8,11 @@ cd  "${TRAVIS_REPO_SLUG}-bench" && \
 
 # Bench master
 git checkout master && \
-cargo bench --bench benchmark -- --noplot --save-baseline before && \
+cargo bench -- --noplot --save-baseline before && \
 
 # Bench current branch
 git checkout ${TRAVIS_COMMIT} && \
-cargo bench --bench benchmark -- --noplot --save-baseline after && \
+cargo bench -- --noplot --save-baseline after && \
 
 # Install https://github.com/BurntSushi/critcmp
 cargo install critcmp --force && \
