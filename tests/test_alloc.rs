2c17eff906071177884ca24f3f9302708f9f25cc
sharksforarms <sharksforarms@users.noreply.github.com>
2019-09-16 07:51:33 -0400

    test alloc ipv4

--- tests/test_alloc.rs
+++ tests/test_alloc.rs
@@ -8,13 +8,24 @@ static A: AllocCounterSystem = AllocCounterSystem;
 mod tests {
 
     use alloc_counter::count_alloc;
-    use rust_packet::layer::{ether::Ether, Layer};
+    use rust_packet::layer::{ether::Ether, ip::Ipv4, Layer};
 
     #[test]
-    fn test_alloc() {
+    fn test_alloc_ether_from_bytes() {
         assert_eq!(
             count_alloc(|| {
-                let _ether = Ether::from_bytes(b"12345678901234");
+                let _ether = Ether::from_bytes(b"ec086b507d584ccc6ad61f760800FFFF");
+            })
+            .0,
+            (0, 0, 0)
+        );
+    }
+
+    #[test]
+    fn test_alloc_ipv4_from_bytes() {
+        assert_eq!(
+            count_alloc(|| {
+                let _ip = Ipv4::from_bytes(b"450000502bc1400040068f37c0a8016bc01efd7dFFFF");
             })
             .0,
             (0, 0, 0)
