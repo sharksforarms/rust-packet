ff12d3c4aa02e2bf8f11fd71aee127089e6e45e1
sharks <sharks@localhost.localdomain>
2019-09-16 20:27:58 -0500

    try this

--- after-success.sh
+++ after-success.sh
@@ -21,7 +21,7 @@ cargo install critcmp --force
 CRITCMP_OUT=`critcmp before after | sed -z 's#\n#<br/>#g'`;
 read -d '' DATA_JSON << EOF
 {
-    "body": "Benchmarks: $(date -u)\r\n\r\n\`\`\`text\r\n$CRITCMP_OUT\r\n\`\`\`"
+    "body": "Benchmarks: $(date -u)<br/><br/>```text $CRITCMP_OUT```"
 }
 EOF
 
