82d6f85f87b42bbb3a55e17371ac2ce04e7e0209
sharksforarms <sharksforarms@users.noreply.github.com>
2019-09-17 20:29:27 -0400

    gave up on this for now, see other branch

--- after-success.sh
+++ after-success.sh
@@ -17,48 +17,4 @@ cargo bench --bench benchmark -- --noplot --save-baseline after && \
 # Install https://github.com/BurntSushi/critcmp
 cargo install critcmp --force && \
 
-# Compare the two generated benches
-CRITCMP_OUT="$(critcmp before after)";
-
-read -d '' DATA_JSON << EOF
-{
-    "body": "Benchmarks: $(date -u)
-
-```text
-$CRITCMP_OUT
-```
-"
-}
-EOF
-
-echo "$DATA_JSON" > data.json
-cat data.json
-
-# Post github comment with results of benchmark
-if [ "${TRAVIS_REPO_SLUG}" == "sharksforarms/rust-packet" ]; then
-    COMMENTS_API=""https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-
-    # Get existing comment if exists
-    COMMENT_URL=$(curl $COMMENTS_API | jq "
-    .[] | \
-        if (.body | startswith(\"Benchmarks:\")) and (.user.login == \"sharksforarmss\") \
-        then \
-            .url \
-        else \
-            error(\"Error: Could not find comment\") \
-        end \
-        | . \
-        ")
-
-    if [ $? -eq 0 ]
-    then
-        # Update the comment
-        curl -vvv -H "Authorization: token ${GITHUB_TOKEN}" -X PATCH \
-        "$COMMENT_URL" \
-        -d @data.json
-    else
-        # Create the comment
-        curl -vvv -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-        "$COMMENTS_API" \
-        -d @data.json
-fi
+critcmp before after
