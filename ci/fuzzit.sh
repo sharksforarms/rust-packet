94b4374deceae6ec69ffe84040a33e4bb546edcf
sharksforarms <sharksforarms@users.noreply.github.com>
2019-10-10 22:15:43 -0400

    try this

--- ci/fuzzit.sh
+++ ci/fuzzit.sh
@@ -1,16 +1,15 @@
 set -xe
 
-FUZZERS=$(TERM="" cargo fuzz list)
 ## build fuzzers
-echo $FUZZERS | xargs -i cargo fuzz run {} -- -runs=0
+TERM="" cargo fuzz list | xargs -i cargo +nightly fuzz run {} -- -runs=0
 
 wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.29/fuzzit_Linux_x86_64
 chmod a+x fuzzit
 
 
-for FUZZER in $(echo $FUZZERS)
+for FUZZER in $(TERM="" cargo fuzz list)
 do
-    if [ $1 == "fuzzing" ]; then
+    if [ "$1" = "fuzzing" ]; then
         ./fuzzit create job fuzzitdev/$FUZZER ./fuzz/target/x86_64-unknown-linux-gnu/debug/$FUZZER
     else
         ./fuzzit create job --type local-regression fuzzitdev/$FUZZER ./fuzz/target/x86_64-unknown-linux-gnu/debug/$FUZZER
