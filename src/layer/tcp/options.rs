e03a3c2d3254a09db7ac7eb38ebd07c816e7d25d
Emmanuel Thompson <eet6646@gmail.com>
2020-06-29 12:53:05 -0400

    feat: Add better testing for TCP option parsing

--- src/layer/tcp/options.rs
+++ src/layer/tcp/options.rs
@@ -46,3 +46,60 @@ pub enum TcpOption {
     #[deku(id = "0x08")]
     Timestamp { length: u8, value: TimestampData },
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex_literal::hex;
+    use rstest::rstest;
+    use std::convert::TryFrom;
+
+    #[rstest(input, expected,
+        case(&hex!("00"), TcpOption::EOL),
+        case(&hex!("01"), TcpOption::NOP),
+        case(&hex!("0201BBAA"), TcpOption::MSS { length: 0x01, value: 0xBBAA }),
+        case(&hex!("0301AA"), TcpOption::WScale { length: 0x01, value: 0xAA }),
+        case(&hex!("0401"), TcpOption::SAckOK { length: 0x01 }),
+        case(&hex!("050ae4d6c0f0e4d6cba0"), TcpOption::SAck {
+            length: 10,
+            value: vec![SAckData { begin: 3839279344, end: 3839282080 }]
+        }),
+        case(&hex!("080ad3845879407337de"), TcpOption::Timestamp {
+            length: 10,
+            value: TimestampData {
+                start: 3548665977,
+                end: 1081292766
+            }
+        }),
+
+
+        // Errors
+        #[should_panic(expected = "Parse(\"overflow when parsing SAckData vec\")")]
+        case::sack_length_underflow(&hex!("0500e4d6c0f0e4d6cba0"), TcpOption::EOL),
+    )]
+    fn test_tcp_option(input: &[u8], expected: TcpOption) {
+        let option = TcpOption::try_from(input).unwrap();
+        assert_eq!(expected, option);
+    }
+
+    #[rstest(sack_len,
+        case::max(31),
+
+        #[should_panic(expected = "Parse(\"error parsing int: out of range integral type conversion attempted\")")]
+        case::overflow(32),
+    )]
+    fn test_sack_update_overflow(sack_len: usize) {
+        let mut sack = TcpOption::SAck {
+            length: 0,
+            value: vec![SAckData { begin: 0, end: 0 }; sack_len],
+        };
+
+        sack.update().unwrap();
+
+        if let TcpOption::SAck { length, value } = sack {
+            assert_eq!(value.len() * 8 + 2, length as usize);
+        } else {
+            unreachable!()
+        }
+    }
+}
