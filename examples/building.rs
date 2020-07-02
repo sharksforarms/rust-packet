b592004f177b32cd11bd6c6835407b6570009409
Emmanuel Thompson <eet6646@gmail.com>
2020-07-02 17:31:03 -0400

    fix: pkt! macro and add examples
    - Fix pkt! macro, call update on creation to update lengths/checksums
    - Updated README to cleanup and reflect example

---
+++ examples/building.rs
@@ -0,0 +1,21 @@
+use rust_packet::prelude::*;
+
+fn main() {
+    let my_pkt = pkt! {
+        ether! {
+            dst: "da:af:d8:5f:73:f8".parse()?
+        }?,
+        ipv4! {
+            src: "127.0.0.1".parse()?,
+            dst: "127.0.0.2".parse()?,
+        }?,
+        udp! {
+            dport: 53
+        }?,
+        raw! {
+            data: b"hello world".to_vec()
+        }?,
+    }.unwrap();
+
+    let _raw_bytes = my_pkt.to_bytes();
+}
