ec7f06c3444726a541562de545a0aa67d3edfbbe
Emmanuel Thompson <eet6646@gmail.com>
2020-07-02 19:26:20 -0400

    try this

--- src/lib.rs
+++ src/lib.rs
@@ -8,43 +8,43 @@ See [examples](https://github.com/sharksforarms/rust-packet/tree/master/examples
 ```rust
 use rust_packet::prelude::*;
 
-fn main() {
-    // Build a packet!
-    let pkt: Packet = pkt! {
-        ether! {
-            dst: "de:ad:be:ef:c0:fe".parse()?
-        }?,
-        ipv4! {
-            src: "127.0.0.1".parse()?,
-            dst: "127.0.0.2".parse()?,
-        }?,
-        udp! {
-            dport: 1337
-        }?,
-        raw! {
-            data: b"hello world!".to_vec()
-        }?,
-    }.unwrap();
+# fn main() {
+// Build a packet!
+let pkt: Packet = pkt! {
+    ether! {
+        dst: "de:ad:be:ef:c0:fe".parse()?
+    }?,
+    ipv4! {
+        src: "127.0.0.1".parse()?,
+        dst: "127.0.0.2".parse()?,
+    }?,
+    udp! {
+        dport: 1337
+    }?,
+    raw! {
+        data: b"hello world!".to_vec()
+    }?,
+}.unwrap();
 
-    // Read a packet!
-    let input: Vec<u8> = pkt.to_bytes().unwrap();
-    let mut pkt = Packet::from_bytes(input.as_ref()).unwrap();
+// Read a packet!
+let input: Vec<u8> = pkt.to_bytes().unwrap();
+let mut pkt = Packet::from_bytes(input.as_ref()).unwrap();
 
-    // Change the packet!
-    if let Some(ipv4) = pkt.ipv4_mut() {
-        ipv4.dst = "127.0.0.3".parse().unwrap()
-    }
-
-    // Update the packet! (This will update the various checksums (Ipv4, TCP, UDP))
-    pkt.update().unwrap();
-
-    // Write the packet!
-    let raw_bytes = pkt.to_bytes().unwrap();
+// Change the packet!
+if let Some(ipv4) = pkt.ipv4_mut() {
+    ipv4.dst = "127.0.0.3".parse().unwrap()
 }
+
+// Update the packet! (This will update the various checksums (Ipv4, TCP, UDP))
+pkt.update().unwrap();
+
+// Write the packet!
+let raw_bytes = pkt.to_bytes().unwrap();
+# }
 ```
 */
 
-#[allow(clippy::needless_doctest_main)]
+// #[allow(clippy::needless_doctest_main)]
 pub mod layer;
 pub mod packet;
 pub mod prelude;
